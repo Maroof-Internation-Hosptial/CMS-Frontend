82a40c40226d87315624a91770349d622c98fc49
Spark Lab <ceo@sparklab.pk>
2024-02-24 11:01:35 +0500

    minor changes

--- src/componenets/pages/admin_event_direcotory.jsx
+++ src/componenets/pages/admin_event_direcotory.jsx
@@ -58,7 +58,7 @@ const Eventdirectory = () => {
             <div className="container-fluid">
               <div className="row mb-2">
                 <div className="col-sm-6">
-                  <h1>Complaints</h1>
+                 
                 </div>
                 <div className="col-sm-6">
                   <ol className="breadcrumb float-sm-right">
--- src/componenets/pages/muawin_event_directory.jsx
+++ src/componenets/pages/muawin_event_directory.jsx
@@ -41,7 +41,7 @@ const Eventdirectory = () => {
 						<div className="container-fluid">
 							<div className="row mb-2">
 								<div className="col-sm-6">
-									<h1>Complaints</h1>
+									
 								</div>
 								<div className="col-sm-6">
 									<ol className="breadcrumb float-sm-right">
