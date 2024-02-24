5dd3d534bf2d9fd5e1588bb2593fceb735ebfd64
Spark Lab <ceo@sparklab.pk>
2024-02-24 12:55:37 +0500

    button location switched

--- src/componenets/pages/updateevent.jsx
+++ src/componenets/pages/updateevent.jsx
@@ -9,200 +9,200 @@ import { Link } from "react-router-dom";
 import Loader from "../Loader";
 import { toast } from "sonner";
 import {
-	useSelectiveAssigneeQuery,
-	useUpdateEventMutation,
+  useSelectiveAssigneeQuery,
+  useUpdateEventMutation,
 } from "../../api/api";
 
 const Updateevent = () => {
-	const [update, updateResp] = useUpdateEventMutation();
-	const [assignees, setAssignees] = useState([]);
-	const { data: detail, isLoading } = useSelectiveAssigneeQuery();
-	useEffect(() => {
-		setAssignees(detail);
-	}, [detail]);
+  const [update, updateResp] = useUpdateEventMutation();
+  const [assignees, setAssignees] = useState([]);
+  const { data: detail, isLoading } = useSelectiveAssigneeQuery();
+  useEffect(() => {
+    setAssignees(detail);
+  }, [detail]);
 
-	const departments = [
-		{ value: "M012700", label: "MIS & Communication" },
-		{ value: "M014004", label: "Facility Management Department" },
-		{ value: "M011500", label: "Food & Nutrition Services (FNSD)" },
-		{ value: "M013200", label: "Bio Medical" },
-	];
+  const departments = [
+    { value: "M012700", label: "MIS & Communication" },
+    { value: "M014004", label: "Facility Management Department" },
+    { value: "M011500", label: "Food & Nutrition Services (FNSD)" },
+    { value: "M013200", label: "Bio Medical" },
+  ];
 
-	const { state } = useLocation();
+  const { state } = useLocation();
 
-	const [data, setData] = useState({
-		name: state.event.name,
-		description: state.event.description,
-		status: state.event.status,
-		priority: state.event.priority,
-		department: state.event.department,
-		assignedTo: state.event.assignedTo,
-	});
-	const [files, setFiles] = useState(state.event.files);
+  const [data, setData] = useState({
+    name: state.event.name,
+    description: state.event.description,
+    status: state.event.status,
+    priority: state.event.priority,
+    department: state.event.department,
+    assignedTo: state.event.assignedTo,
+  });
+  const [files, setFiles] = useState(state.event.files);
 
-	function handleDelete(name) {
-		setFiles((prev) => prev.filter((file) => file.name !== name));
-	}
+  function handleDelete(name) {
+    setFiles((prev) => prev.filter((file) => file.name !== name));
+  }
 
-	const onChange = (e) => {
-		setData({ ...data, [e.target.name]: e.target.value });
-	};
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-	function handleUpdate() {
-		update({ id: state.event._id, data: { ...data, files: [...files] } }).then(
-			(res) => {
-				if (res?.data?.message) {
-					toast.success("Event Updated Successfully!");
-				}
-			}
-		);
-	}
+  function handleUpdate() {
+    update({ id: state.event._id, data: { ...data, files: [...files] } }).then(
+      (res) => {
+        if (res?.data?.message) {
+          toast.success("Event Updated Successfully!");
+        }
+      }
+    );
+  }
 
-	return (
-		<div className="wrapper">
-			<Navbar />
-			<div style={{ padding: "0 20px" }}>
-				{/* Content Header (Page header) */}
-				<section className="content-header">
-					<div className="container-fluid">
-						<div className="row mb-2">
-							<div className="col-sm-6">
-								<h1>Event Edit</h1>
-								{/* <p>{JSON.stringify(assignees, null, 4)}</p> */}
-							</div>
-							<div className="col-sm-6">
-								<ol className="breadcrumb float-sm-right">
-									<li className="breadcrumb-item">
-										<a href="/dashboard">Home</a>
-									</li>
-									<li className="breadcrumb-item ">Edit Event</li>
-								</ol>
-							</div>
-						</div>
-					</div>
-					{/* /.container-fluid */}
-				</section>
-				{/* Main content */}
-				<section className="content">
-					<div className="row">
-						<div className="col-md-12">
-							<div className="card card-primary">
-								<div className="card-header">
-									<h3 className="card-title">General</h3>
-									<div className="card-tools">
-										<button
-											type="button"
-											className="btn btn-tool"
-											data-card-widget="collapse"
-											data-toggle="tooltip"
-											title="Collapse"
-										>
-											<i className="fas fa-minus" />
-										</button>
-									</div>
-								</div>
-								<div className="card-body">
-									<div className="form-group">
-										<label htmlFor="inputName">Subject</label>
-										<input
-											name="name"
-											type="text"
-											id="inputName"
-											className="form-control"
-											value={data?.name}
-											onChange={onChange}
-											readOnly={true}
-										/>
-									</div>
-									<div className="form-group">
-										<label htmlFor="inputDescription">Description</label>
-										<textarea
-											name="description"
-											id="inputDescription"
-											className="form-control"
-											rows={4}
-											value={data?.description}
-											onChange={onChange}
-											readOnly={true} // Set readOnly attribute to true
-										/>
-									</div>
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <div style={{ padding: "0 20px" }}>
+        {/* Content Header (Page header) */}
+        <section className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1>Event Edit</h1>
+                {/* <p>{JSON.stringify(assignees, null, 4)}</p> */}
+              </div>
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item">
+                    <a href="/dashboard">Home</a>
+                  </li>
+                  <li className="breadcrumb-item ">Edit Event</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          {/* /.container-fluid */}
+        </section>
+        {/* Main content */}
+        <section className="content">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-primary">
+                <div className="card-header">
+                  <h3 className="card-title">General</h3>
+                  <div className="card-tools">
+                    <button
+                      type="button"
+                      className="btn btn-tool"
+                      data-card-widget="collapse"
+                      data-toggle="tooltip"
+                      title="Collapse"
+                    >
+                      <i className="fas fa-minus" />
+                    </button>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="form-group">
+                    <label htmlFor="inputName">Subject</label>
+                    <input
+                      name="name"
+                      type="text"
+                      id="inputName"
+                      className="form-control"
+                      value={data?.name}
+                      onChange={onChange}
+                      readOnly={true}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="inputDescription">Description</label>
+                    <textarea
+                      name="description"
+                      id="inputDescription"
+                      className="form-control"
+                      rows={4}
+                      value={data?.description}
+                      onChange={onChange}
+                      readOnly={true} // Set readOnly attribute to true
+                    />
+                  </div>
 
-									<div className="row">
-										<div className="form-group col-md-3">
-											<label htmlFor="inputDepartment">
-												Complaint Department{" "}
-											</label>
-											<input
-												name="department"
-												id="inputDepartment"
-												className="form-control custom-select"
-												type="text"
-												value={data?.department}
-												readOnly // Make the input non-editable
-											/>
-										</div>
+                  <div className="row">
+                    <div className="form-group col-md-3">
+                      <label htmlFor="inputDepartment">
+                        Complaint Department{" "}
+                      </label>
+                      <input
+                        name="department"
+                        id="inputDepartment"
+                        className="form-control custom-select"
+                        type="text"
+                        value={data?.department}
+                        readOnly // Make the input non-editable
+                      />
+                    </div>
 
-										<div className="form-group col-md-3">
-											<label htmlFor="inputPriority">Priority</label>
-											<select
-												name="priority"
-												className="form-control custom-select"
-												value={data?.priority}
-												onChange={onChange}
-											>
-												<option value="" defaultValue="" selected disabled>
-													Select Priority
-												</option>
-												<option value="Low">Low</option>
-												<option value="Medium">Medium</option>
-												<option value="High">High</option>
-											</select>
-										</div>
+                    <div className="form-group col-md-3">
+                      <label htmlFor="inputPriority">Priority</label>
+                      <select
+                        name="priority"
+                        className="form-control custom-select"
+                        value={data?.priority}
+                        onChange={onChange}
+                      >
+                        <option value="" defaultValue="" selected disabled>
+                          Select Priority
+                        </option>
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                      </select>
+                    </div>
 
-										<div className="form-group col-md-3">
-											<label htmlFor="inputStatus">Status</label>
-											<select
-												name="status"
-												className="form-control custom-select"
-												value={data?.status}
-												onChange={onChange}
-											>
-												<option value="" selected disabled>
-													Select one
-												</option>
-												<option value="in-progress">In Progress</option>
-												<option value="canceled">Canceled</option>
-												<option value="resolved">Resolved</option>
-											</select>
-										</div>
+                    <div className="form-group col-md-3">
+                      <label htmlFor="inputStatus">Status</label>
+                      <select
+                        name="status"
+                        className="form-control custom-select"
+                        value={data?.status}
+                        onChange={onChange}
+                      >
+                        <option value="" selected disabled>
+                          Select one
+                        </option>
+                        <option value="in-progress">In Progress</option>
+                        <option value="canceled">Canceled</option>
+                        <option value="resolved">Resolved</option>
+                      </select>
+                    </div>
 
-										<div className="form-group col-md-3">
-											<label htmlFor="inputStatus">Assign To</label>
-											<select
-												name="assignedTo"
-												className="form-control custom-select"
-												value={data?.assignedTo}
-												onChange={onChange}
-											>
-												<option value="" selected disabled>
-													Select one
-												</option>
-												{assignees?.map((assignee) => (
-													<>
-														<option value={assignee._id}>
-															{assignee.firstName + " " + assignee.lastName}
-														</option>
-													</>
-												))}
-											</select>
-										</div>
-									</div>
-								</div>
-								{/* /.card-body */}
-							</div>
-							{/* /.card */}
-						</div>
-						{/* <div className="col-md-12">
+                    <div className="form-group col-md-3">
+                      <label htmlFor="inputStatus">Assign To</label>
+                      <select
+                        name="assignedTo"
+                        className="form-control custom-select"
+                        value={data?.assignedTo}
+                        onChange={onChange}
+                      >
+                        <option value="" selected disabled>
+                          Select one
+                        </option>
+                        {assignees?.map((assignee) => (
+                          <>
+                            <option value={assignee._id}>
+                              {assignee.firstName + " " + assignee.lastName}
+                            </option>
+                          </>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                {/* /.card-body */}
+              </div>
+              {/* /.card */}
+            </div>
+            {/* <div className="col-md-12">
               <div className="card card-info">
                 <div className="card-header">
                   <h3 className="card-title">Files</h3>
@@ -259,36 +259,37 @@ const Updateevent = () => {
                 </div>
               </div>
             </div> */}
-					</div>
-					<div className="row" id="cap">
-						<Link
-							to="/eventdirectory"
-							className="btn btn-secondary"
-							style={{ marginRight: "10px", marginLeft: "10px" }}
-						>
-							Cancel
-						</Link>
+          </div>
+          <div className="row" id="cap">
+            {updateResp.isLoading ? (
+              <div className="float-right" style={{ marginRight: 15 }}>
+                <Loader size={30} />
+              </div>
+            ) : (
+              <button
+                onClick={handleUpdate}
+                type="button"
+                className="btn btn-success float-right"
+                style={{ marginRight: "10px", marginLeft: "10px" }}
+              >
+                Save Changes
+              </button>
+            )}
 
-						{updateResp.isLoading ? (
-							<div className="float-right" style={{ marginRight: 15 }}>
-								<Loader size={30} />
-							</div>
-						) : (
-							<button
-								onClick={handleUpdate}
-								type="button"
-								className="btn btn-success float-right"
-							>
-								Save Changes
-							</button>
-						)}
-					</div>
-				</section>
-				{/* /.content */}
-			</div>
-			<Footer />
-		</div>
-	);
+            <Link
+              to="/eventdirectory"
+              className="btn btn-secondary"
+              style={{ marginRight: "10px", marginLeft: "10px" }}
+            >
+              Cancel
+            </Link>
+          </div>
+        </section>
+        {/* /.content */}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Updateevent;
